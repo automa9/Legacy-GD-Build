3ef8671e6545b05897e5e81dcd3534bb1e733cc2
Danial Azwar <33915658+automa9@users.noreply.github.com>
2023-09-22 00:57:11 +0800

    texture light

--- Player.gd
+++ Player.gd
@@ -1,30 +1,44 @@
 extends CharacterBody2D
+#Accessable via Editor
+@export var speed = 300 
+@export var gravity = 30
+@export var jump_force = 200
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
+@onready var animation_player = $AnimationPlayer
+@onready var sprite = $Sprite2D
+#physics update
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if !is_on_floor():
+		velocity.y += gravity
+		if velocity.y > 1000:
+			velocity.y = 1000
+			
+	if Input.is_action_just_pressed("ui_up") && is_on_floor():
+			velocity.y = -jump_force
 		
-	# Flip the sprite
-
+	var horizontal_direction  = Input.get_axis("ui_left","ui_right")
+	velocity.x  = speed * horizontal_direction
+	if horizontal_direction != 0:
+		switch_direction(horizontal_direction)
+		#sprite.flip_h = (horizontal_direction==-1)
+		
 	move_and_slide()
+	
+	print(velocity)
+	update_animations(horizontal_direction)
+	
+func update_animations(horizontal_direction):
+	if is_on_floor():
+			if horizontal_direction == 0 :
+				animation_player.play("Idle")
+			else:
+				animation_player.play("Run")
+	else:
+		if velocity.y < 0:
+			animation_player.play("Jump")
+		elif velocity.y >0:
+			animation_player.play("Fall")
+
+func switch_direction(horizontal_direction):
+	sprite.flip_h = (horizontal_direction ==-1)
+	sprite.position.x = horizontal_direction *4
